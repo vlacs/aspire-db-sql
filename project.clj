3c9a109809178cb41c124a1b10cea2c3f246dd0a
Matt Oquist <moquist@majen.net>
2013-11-20 13:45:17 -0500

    Correct version string; eliminate :dev profile.

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject jdbc-pg-init "0.1.1-SNAPSHOT1"
+(defproject jdbc-pg-init "0.1.2-SNAPSHOT"
   :description "A library to create PostgreSQL tables and indices from edn data."
   :url "https://github.com/vlacs/jdbc-pg-init"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.0-beta1"]]
-  :profiles
-  {:dev
-   {:dependencies [[postgresql/postgresql "8.4-702.jdbc4"]
-                   [mysql/mysql-connector-java "5.1.26"]]}})
+                 [org.clojure/java.jdbc "0.3.0-beta1"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]])
