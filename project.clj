8607aa5ec381176605e18a21834d3c88dba893a7
Matt Oquist <moquist@majen.net>
2013-11-20 12:53:21 -0500

    dep on stock upstream java.jdbc

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
-(defproject jdbc-pg-init "0.1.1-SNAPSHOT"
+(defproject jdbc-pg-init "0.1.1-SNAPSHOT1"
   :description "A library to create PostgreSQL tables and indices from edn data."
   :url "https://github.com/vlacs/jdbc-pg-init"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;; Patch sent to upstream: http://dev.clojure.org/jira/browse/JDBC-73
-                 ;; Until patch is accepted, this can be grabbed from
-                 ;; https://github.com/moquist/java.jdbc and installed locally
-                 [org.clojure/java.jdbc "0.3.0-SNAPSHOT4"]]
+                 [org.clojure/java.jdbc "0.3.0-beta1"]]
   :profiles
   {:dev
    {:dependencies [[postgresql/postgresql "8.4-702.jdbc4"]
